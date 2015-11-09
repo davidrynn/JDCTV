1413fb70df3ca4b933424f5dcac57394956bc54f
davidrynn <davidrynn@gmail.com>
2015-11-09 11:51:45 -0500

    added extension so images show up in main menu

--- JDCPreview/ServiceProvider.swift
+++ JDCPreview/ServiceProvider.swift
@@ -19,13 +19,22 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
 
     var topShelfStyle: TVTopShelfContentStyle {
         // Return desired Top Shelf style.
-        return .Sectioned
+        return .Inset
     }
 
     var topShelfItems: [TVContentItem] {
         // Create an array of TVContentItems.
-        return []
+        let item1 = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "JDCTV.item1", container: nil)!)
+        item1?.imageURL = NSURL(string: "https://tctechcrunch2011.files.wordpress.com/2015/01/jet.png?w=680&h=394")
+        let item2 = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "JDCTV.item1", container: nil)!)
+
+        item2?.imageURL = NSURL(string: "http://cdn2.hubspot.net/hub/427956/hubfs/jet-1.jpg?t=1441885473008&width=493")
+        item2?.imageShape = TVContentItemImageShape.Square
+        return [item1!, item2!]
     }
+    
 
 }
+
+
 
